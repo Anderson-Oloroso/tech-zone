da51ee3a5958782ceaa9ba6a6fb4770f7515dfe1
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-02 08:53:52 -0600

    :sparkles: feat(procedure): Add script of procedures

---
+++ script/dml/procedures.sql
@@ -0,0 +1,22 @@
+CREATE PROCEDURE sp_crear_venta(p_cliente_id INT, p_detalle INT, p_precio NUMERIC(8,2))
+LANGUAGE plpgsql AS $$
+DECLARE mensaje VARCHAR(100);
+RETURNS VARCHAR(10);
+BEGIN
+
+    IF p_cliente_id <> (SELECT cliente_id FROM clientes WHERE cliente_id = p_cliente_id) THEN 
+        SET mensaje :='Cliente inexistente';
+    END IF;
+
+    IF p_detalle <> (SELECT detalle_venta_id FROM detalle_venta WHERE detalle_venta WHERE detalle_venta_id = p_detalle) THEN
+        SET mensaje := 'Detalle de venta inexistente';
+    END IF;
+
+    INSERT INTO ventas(cliente_id, detalle_venta_id, precio_final)
+        VALUES
+            (p_cliente_id, p_detalle, p_precio);
+        SET mensaje:= 'Venta creada';
+
+    RETURN mensaje;
+END;
+$$;
