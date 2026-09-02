48b336ba9a59915d2a9178fe634506b6bb0bffdd
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-02 08:11:46 -0600

    :sparkles: feat: Add fks

--- script/ddl/db.sql
+++ script/ddl/db.sql
@@ -10,7 +10,7 @@ CREATE TABLE categorias(
 CREATE TABLE productos(
     producto_id SERIAL PRIMARY KEY,
     nombre VARCHAR(50),
-    categoria_id INT,
+    categoria_id INT REFERENCES categorias(categoria_id) ON DELETE,
     precio NUMERIC(8,2) NOT NULL CHECK(precio > 0),
     stock INT CHECK(stock > 0)
 );
@@ -19,13 +19,13 @@ CREATE TABLE proveedores(
     proveedor_id SERIAL PRIMARY KEY,
     nombre VARCHAR(30) NOT NULL,
     apellido VARCHAR(30),
-    producto_id INT,
+    producto_id INT REFERENCES productos(producto_id) ON DELETE CASCADE,
     email VARCHAR(50) UNIQUE NOT NULL,
     telefono VARCHAR(8)
 );
 
 CREATE TABLE clientes(
-    detalle_venta_id SERIAL PRIMARY KEY,
+    cliente_id SERIAL PRIMARY KEY,
     nombre VARCHAR(30) NOT NULL,
     apellido VARCHAR(30) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
@@ -34,15 +34,15 @@ CREATE TABLE clientes(
 
 CREATE TABLE ventas(
     venta_id SERIAL PRIMARY KEY,
-    cliente_id INT NOT NULL,
-    detalle_venta_id INT NOT NULL,
+    cliente_id INT NOT NULL REFERENCES clientes(cliente_id) ON DELETE CASCADE,
+    detalle_venta_id INT NOT NULL REFERENCES detalle_venta(detalle_venta_id) ON DELETE CASCADE,
     precio_final NUMERIC(8,2) NOT NULL
 );
 
 CREATE TABLE detalle_venta(
     detalle_venta_id SERIAL PRIMARY KEY,
-    producto_id INT NOT NULL, 
-    cliente_id INT NOT NULL,
+    producto_id INT NOT NULL REFERENCES productos(producto_id) ON DELETE CASCADE,
+    cliente_id INT NOT NULL REFERENCES clientes(cliente_id) ON DELETE CASCADE,
     cantidad INT CHECK(cantidad > 0),
     subtotal NUMERIC(8,2)
 );
